2030cf44edcf8080ca254b2338f1acd1ba5ac075
Aaron Soto <aaron.m.soto1@gmail.com>
2022-05-05 15:37:39 -0700

    fixed cookie error

--- src/components/CookiesConsent.js
+++ src/components/CookiesConsent.js
@@ -2,21 +2,21 @@ import React from 'react';
 
 export const CookiesConsent = () => {
 	return (
-		<div class='col-md-4 col-sm-12 button-fixed-cookies'>
-			<div class='p-3 pb-4 bg-custom-cookies text-white'>
-				<div class='row'>
-					<div class='col-10'>
+		<div className='col-md-4 col-sm-12 button-fixed-cookies'>
+			<div className='p-3 pb-4 bg-custom-cookies text-white'>
+				<div className='row'>
+					<div className='col-10'>
 						<h3 className='text-light'>Allow Cookies</h3>
 					</div>
-					<div class='col-2 text-center'>
-						<i class='fas fa-times'></i>
+					<div className='col-2 text-center'>
+						<i className='fas fa-times'></i>
 					</div>
 				</div>
 				<p>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
 					eiusmod tempor incididunt ut labore et dolore magna aliqua.
 				</p>
-				<button type='button' class='btn w-100 text-light'>
+				<button type='button' className='btn w-100 text-light'>
 					Accept Cookies
 				</button>
 			</div>
